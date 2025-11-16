8e7cf83e9fdeee77fc3191ca5421a3516cd1e369
godzilla <225238222+mohian-dev@users.noreply.github.com>
2025-11-16 14:15:01 +0600

    direct route to login page

--- bdms/accounts/urls.py
+++ bdms/accounts/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 
     # Template endpoints
     path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
 ]
--- bdms/bdms/urls.py
+++ bdms/bdms/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
     path('accounts/', include('accounts.urls')),
     path('donations/', include('donations.urls')),
 ]
